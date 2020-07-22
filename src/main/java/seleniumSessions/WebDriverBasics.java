f84871d6feae1917b5565f346f841c465a5810e8
seana <seana.k189@gmail.com>
2020-07-22 19:31:16 +0530

    some minor changes

--- src/main/java/seleniumSessions/WebDriverBasics.java
+++ src/main/java/seleniumSessions/WebDriverBasics.java
@@ -4,29 +4,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverBasics {
+	/*
+	 * @author seanak
+	 * In comments you can see Manual testing Points but after Authentication manual testing covert to Automation Testing 
+	 * Goal of Automation tester is to provide validation points to normal manual testing and catch errors 
+	 */
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:/chromeDriver/chromedriver_win32/chromedriver.exe");
 		
-		WebDriver driver = new ChromeDriver(); //1. open browser
+		//1. open browser
+		WebDriver driver = new ChromeDriver(); 
 		
-		driver.get("https://www.google.com"); //2.enter url
+		//2.enter url
+		driver.get("https://www.google.com"); 
 		
-		String title = driver.getTitle(); // get the page title 
+		// get the page title 
+		String title = driver.getTitle(); 
 		System.out.println("page title is - " +title);
 		
-		// validation point
-		
+		// validation of title 
 		if(title.equals("Google")){   // verification of title 
 			System.out.println("correct title");
 		}else {
 			System.out.println("in-correct title");
 		}
 		
-		System.out.println("current url = "+driver.getCurrentUrl());
+		//get current url
+		String currentUrl = driver.getCurrentUrl(); 
+		
+		System.out.println("current url = "+ currentUrl);
+		
+		//verify current url
+		if (currentUrl.equals("https://www.google.com/")){
+			System.out.println("current url is correct");
+		}else {
+			System.out.println("current url is in-correct");
+		}
 		
 		//close the browser 
-		
 		driver.quit(); //session id is null
 		//driver.close(); // session id is there but invalid 
 		
