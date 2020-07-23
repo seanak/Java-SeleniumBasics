9440d173275a480a9255fb060b33b7133323ee81
seana <seana.k189@gmail.com>
2020-07-23 20:31:41 +0530

    WindowPopUpHandles.java added to repo

---
+++ src/main/java/seleniumSessions/F_WindowPopUpHandles.java
@@ -0,0 +1,52 @@
+package seleniumSessions;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class F_WindowPopUpHandles {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://popuptest.com/goodpopups.html");
+		
+		driver.findElement(By.linkText("Good PopUp #1")).click();
+		
+		Set<String> Handles =driver.getWindowHandles();
+		
+		Iterator<String> it = Handles.iterator();
+		
+		String parentWindID = it.next();
+		
+		System.out.println("parent window id is "+ parentWindID);
+		
+		String childwindId = it.next();
+		
+		System.out.println("child window id is:  "+childwindId);
+		
+		driver.switchTo().window(childwindId);
+		
+		String cw1Title = driver.getTitle();
+		
+		System.out.println("child window title is : "+cw1Title);
+		
+		driver.close();
+		
+		driver.switchTo().window(parentWindID);
+		
+		String pwTitle = driver.getTitle();
+		
+		System.out.println("parent window title is : "+pwTitle);
+		
+		driver.quit();
+
+	}
+
+}
