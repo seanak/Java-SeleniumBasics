976fd41c372b1b0e701e772661daaaf941653ac2
seana <seana.k189@gmail.com>
2020-07-24 03:15:27 +0530

    with Attribute get value from text field added to repo

---
+++ src/main/java/seleniumSessions/Ga_WithAttributeGetValueFromTextField.java
@@ -0,0 +1,27 @@
+package seleniumSessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Ga_WithAttributeGetValueFromTextField {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+	
+		driver.get("https://register.rediff.com/register/register.php");
+		
+		driver.findElement(By.xpath("//*[@id='tblcrtac']/tbody/tr[3]/td[3]/input")).sendKeys("text123@rediffmail.com");
+	
+		String value = driver.findElement(By.xpath("//*[@id='tblcrtac']/tbody/tr[3]/td[3]/input")).getAttribute("value");
+		
+		System.out.println(value);
+		
+		driver.quit();
+	}
+
+}
